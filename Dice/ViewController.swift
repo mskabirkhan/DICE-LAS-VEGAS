68298868c813f9186e3dede90570989c2e9d00bb
Kabir <kabir@kabirs-mbp.mynet>
2018-11-14 00:20:32 +0000

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Kabir on 14/11/2018.
+//  Copyright © 2018 Kabir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
